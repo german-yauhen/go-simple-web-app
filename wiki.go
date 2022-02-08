00d879ef356b63237817c5ceac227279b2dc89ba
Yauheni Hermanovich <german.yauhen@gmail.com>
2022-02-08 11:51:02 +0300

    Introducing the net/http package

--- wiki.go
+++ wiki.go
@@ -2,16 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
+	"net/http"
 	"os"
 	"syscall"
 )
 
 func main() {
-	storedPage := &Page{Title: "firstpage", Body: []byte("The content of the first page")}
-	storedPage.save()
+	// storedPage := &Page{Title: "firstpage", Body: []byte("The content of the first page")}
+	// storedPage.save()
 	
-	loadedPage, _ := loadPage("firstpage")
-	fmt.Println(string(loadedPage.Body))
+	// loadedPage, _ := loadPage("firstpage")
+	// fmt.Println(string(loadedPage.Body))
+	http.HandleFunc("/", handleRoot)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
 
 func (pointerToPage *Page) save() error {
@@ -26,6 +30,11 @@ func loadPage(title string) (*Page, error) {
 		return nil, err
 	}
 	return &Page{title, body}, nil
+}
+
+func handleRoot(rsWriter http.ResponseWriter, rq *http.Request) {
+	// rq.URL.Path[1:] creates a sub-slice of Path from the 1st character to the end, this drops the leading "/" from the path name
+	fmt.Fprintf(rsWriter, "Hi there! I love %s!", rq.URL.Path[1:])
 }
 
 type Page struct {
